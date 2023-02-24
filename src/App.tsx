f9d8c3f20b0493664aa8a4f6c5c98b38fc6ae4f5
Egor Dubovik <egorka54896@gmail.com>
2023-02-24 21:01:40 +0300

    init: create app with CRA and add tsconfig, eslint, prettier, husky

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const a = 5;
+
+  return <div className="App">react app</div>;
+}
+
+export default App;
