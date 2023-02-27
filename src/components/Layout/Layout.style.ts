3c8749f3fef2aea4a9f4c872b350531ba67059ff
Egor Dubovik <egorka54896@gmail.com>
2023-02-28 00:47:16 +0300

    feet: add commponents Layout and Footer

---
+++ src/components/Layout/Layout.style.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from '@mui/styles';
+
+const useLayoutStyles = makeStyles({
+  main: {
+    padding: '70px  0 30px 0',
+    flex: '1 1 auto',
+  },
+});
+
+export default useLayoutStyles;
