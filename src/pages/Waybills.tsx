bab639ae516000e6031ebd90db105b6e57e81abb
Egor Dubovik <egorka54896@gmail.com>
2023-02-28 11:51:45 +0300

    feet: add pages Returns, Scanner, Waybills and routes for it

---
+++ src/pages/Waybills.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Waybills = () => {
+  return <>Waybills</>;
+};
+
+export default Waybills;
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,11 +3,26 @@ import MainPage from 'pages/MainPage';
 import Profile from 'pages/Profile';
 import { Navigate } from 'react-router-dom';
 import { ROUTES } from 'constant/navigaton';
+import Returns from 'pages/Returns';
+import Waybills from 'pages/Waybills';
+import Scanner from 'pages/Scanner';
 
 export const publicRoutes = [
   {
     path: ROUTES.MAIN,
     element: <MainPage />,
+  },
+  {
+    path: ROUTES.RETURNS,
+    element: <Returns />,
+  },
+  {
+    path: ROUTES.WAYBILLS,
+    element: <Waybills />,
+  },
+  {
+    path: ROUTES.SCANNER,
+    element: <Scanner />,
   },
   {
     path: ROUTES.PROFILE,
